ecbc433cb5c693dca583866d305e36a3fa719192
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-11 19:58:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,10 @@
 require 'pry'
 def starts_with_a_vowel?(word)
   binding.pry
+  if word.match(/[aeiou]/) == true
+    return true
+  else
+    return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
